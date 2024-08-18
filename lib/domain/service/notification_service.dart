dedf9efe2700774749b1b54167911e6e1100e7a5
anujbarve <anujbarve27@gmail.com>
2024-08-18 15:25:58 +0530

    updated readme

--- lib/domain/service/notification_service.dart
+++ lib/domain/service/notification_service.dart
@@ -14,25 +14,35 @@ class NotificationService {
     final int hour = timeOfDay.hour;
     final int minute = timeOfDay.minute;
 
+    // Calculate the notification time based on the current time and the desired time
+    final now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduledDate = tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, minute);
+
+    // If the scheduled time is before the current time, schedule it for the next day
+    if (scheduledDate.isBefore(now)) {
+      scheduledDate = scheduledDate.add(Duration(days: 1));
+    }
+
     const AndroidNotificationDetails androidPlatformChannelSpecifics =
     AndroidNotificationDetails('your_channel_id', 'your_channel_name',
         channelDescription: 'your_channel_description',
         importance: Importance.max,
         priority: Priority.high,
-        showWhen: false,
         enableVibration: true,
         playSound: true);
 
     const NotificationDetails platformChannelSpecifics =
     NotificationDetails(android: androidPlatformChannelSpecifics);
 
-    await flutterLocalNotificationsPlugin.periodicallyShow(
+    await flutterLocalNotificationsPlugin.zonedSchedule(
       0,
       'Reminder',
       'Time to check your finances!',
-      RepeatInterval.daily,
+      scheduledDate,
       platformChannelSpecifics,
       androidAllowWhileIdle: true,
+      matchDateTimeComponents: DateTimeComponents.time, // Ensures it repeats daily at the same time
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.wallClockTime,
     );
   }
 }
